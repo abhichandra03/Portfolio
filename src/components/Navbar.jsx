6bff003c6e7d8acb19e30298d734cb6b09ad3829
abhichandra03 <abhichandra03@gmail.com>
2024-04-26 20:38:48 +0530

    resume updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -124,7 +124,7 @@ const Navbar = () => {
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-gray-600">
             <a
               className="flex justify-between items-center w-full text-gray-300"
-              href={require("../assests/Abhishek'sResume.pdf")}
+              href={require("../assests/ResumeAbhishek.pdf")}
               download="Abhishek Chandra's Resume"
             >
               Resume <BsFillPersonLinesFill size={30} />
