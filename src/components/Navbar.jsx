076ae6768349b883dd7d7d4a070cdc705fbbd5e3
abhichandra03 <abhichandra03@gmail.com>
2024-07-02 13:35:32 +0530

    mail

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { FaBars, FaTimes, FaGithub, FaLinkedin } from "react-icons/fa";
 import { HiOutlineMail } from "react-icons/hi";
 import { BsFillPersonLinesFill } from "react-icons/bs";
-import { Link } from "react-scroll";
+import { Link } from "react-scroll/modules";
 import Logo from "../assests/logo.png";
 
 const Navbar = () => {
